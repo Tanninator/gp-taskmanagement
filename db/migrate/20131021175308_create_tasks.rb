6bee0d2bcab2ba52a9dbd80a918099a9880538f1
Bryan Mau <bryanmau1@gmail.com>
2013-10-21 11:08:59 -0700

    Once can now do CRUD actions on tasks.  The view is as basic as possible

---
+++ db/migrate/20131021175308_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.integer :progress
+      t.string :people
+      t.string :notes
+
+      t.timestamps
+    end
+  end
+end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,14 +1,14 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
+  setup do
+    @task = tasks(:one)
+  end
+
   test "should get index" do
     get :index
     assert_response :success
-  end
-
-  test "should get create" do
-    get :create
-    assert_response :success
+    assert_not_nil assigns(:tasks)
   end
 
   test "should get new" do
@@ -16,14 +16,34 @@ class TasksControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get destroy" do
-    get :destroy
+  test "should create task" do
+    assert_difference('Task.count') do
+      post :create, task: { notes: @task.notes, people: @task.people, progress: @task.progress, title: @task.title }
+    end
+
+    assert_redirected_to task_path(assigns(:task))
+  end
+
+  test "should show task" do
+    get :show, id: @task
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
+  test "should get edit" do
+    get :edit, id: @task
     assert_response :success
   end
 
+  test "should update task" do
+    patch :update, id: @task, task: { notes: @task.notes, people: @task.people, progress: @task.progress, title: @task.title }
+    assert_redirected_to task_path(assigns(:task))
+  end
+
+  test "should destroy task" do
+    assert_difference('Task.count', -1) do
+      delete :destroy, id: @task
+    end
+
+    assert_redirected_to tasks_path
+  end
 end
